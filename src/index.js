9173be13d3544b8265f264efc9c6354b02355418
YeliseyevaO <eliseevaolgaf5@gmail.com>
2021-08-29 19:49:18 +0300

    дебаунс

--- src/index.js
+++ src/index.js
@@ -1,32 +1,49 @@
 /*import './sass/main.scss';*/
+import debounce from 'lodash.debounce';
+import pnotify from '@pnotify/core';
 
-/* require('lodash.debounce');*/
 const refs = {
     input: document.querySelector('#countryField'),
-    countryList: document.querySelector('.container'),
+  countryList: document.querySelector('.container'),
+    note:document.querySelector('.note'),
 };
-console.dir(refs.input);
-refs.input.addEventListener('input', foundCountry);/*_.debounce(foundCountry, 500));*/
+ /*pnotify.alert({
+    text: 'Notice me, senpai!'
+  });*/
+refs.input.addEventListener('input', debounce(foundCountry, 500));
 
 function foundCountry(e) {
   const countryName = e.target.value;
-  fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
-    .then(response => {
-      return response.json();
-    })
-    .then((countries) => {
-      if (countries.length !== 1) {
+  if (countryName.length > 1) {
+    fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
+      .then(response => {
+        return response.json();
+      })
+      .then((countries) => {
+        if (countries.length > 10) {
+          
+        }
+        return countries;
+      })
+      .then((countries) => {
+        if (countries.length >= 2 && countries.length <= 10) {
           const countriesMarkUp = countries.map(country => `<li>${country.name}</li>`)
-          .join('');
-       return refs.countryList.innerHTML = countriesMarkUp;
-      } else {
-        const countryMarkUp = `<h1>${country.name}<h1>`;
- return refs.countryList.innerHTML = countryMarkUp;
+            .join('');
+          return refs.countryList.innerHTML = countriesMarkUp;
+        }
+        return countries;
       
-      }
-    })
-    .catch(error => {
-      // error handling
-    })
+      })
+      .then((countries) => {
+       if (countries.length === 1) {
+
+          const countryMarkUp = `<h1>${country.name}<h1>`;
+          return refs.countryList.innerHTML = countryMarkUp;
+        }
+      })
+      .catch(error => {
+        // error handling
+      })
+  }
 };
   
