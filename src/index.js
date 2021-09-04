a5fd9a6edde3b6eaa5ea5fefe4fd86189ba6498e
YeliseyevaO <eliseevaolgaf5@gmail.com>
2021-09-04 17:14:52 +0300

    файл API

--- src/index.js
+++ src/index.js
@@ -1,49 +1,43 @@
 /*import './sass/main.scss';*/
 import debounce from 'lodash.debounce';
-import pnotify from '@pnotify/core';
+import { alert } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import '../node_modules/@pnotify/core/dist/PNotify.css';
+import '../node_modules/@pnotify/core/dist/BrightTheme.css';
+import countryCardTml from './card.hbs';
+import fetchCountries from './fetchCountries';
 
 const refs = {
-    input: document.querySelector('#countryField'),
+  input: document.querySelector('#countryField'),
   countryList: document.querySelector('.container'),
-    note:document.querySelector('.note'),
 };
- /*pnotify.alert({
-    text: 'Notice me, senpai!'
-  });*/
+
 refs.input.addEventListener('input', debounce(foundCountry, 500));
 
 function foundCountry(e) {
   const countryName = e.target.value;
   if (countryName.length > 1) {
-    fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
-      .then(response => {
-        return response.json();
-      })
-      .then((countries) => {
+    fetchCountries(countryName)
+      .then(countries => {
         if (countries.length > 10) {
-          
+          alert({
+            text: 'Too many matches found. Please enter a  more specific query!',
+            type: 'error',
+            delay: 3000,
+            hide: true,
+          });
         }
-        return countries;
-      })
-      .then((countries) => {
         if (countries.length >= 2 && countries.length <= 10) {
-          const countriesMarkUp = countries.map(country => `<li>${country.name}</li>`)
-            .join('');
-          return refs.countryList.innerHTML = countriesMarkUp;
+          const countriesMarkUp = countries.map(country => `<li>${country.name}</li>`).join('');
+          refs.countryList.innerHTML = countriesMarkUp;
         }
-        return countries;
-      
-      })
-      .then((countries) => {
-       if (countries.length === 1) {
-
-          const countryMarkUp = `<h1>${country.name}<h1>`;
-          return refs.countryList.innerHTML = countryMarkUp;
+        if (countries.length === 1) {
+          const countryMarkUp = countryCardTml(countries[0]);
+          console.log(countryMarkUp);
+          refs.countryList.innerHTML = countryMarkUp;
         }
       })
       .catch(error => {
         // error handling
-      })
-  }
-};
-  
+      });
+  } else refs.countryList.innerHTML = '';
+}
